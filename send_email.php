8944bfd9197ab2b3e996142f522ea2cb21d44f28
Idris110 <ratlamwalaidris110@gmail.com>
2023-08-15 23:55:07 +0530

    update email id

--- send_email.php
+++ send_email.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $recipient = "*@gmail.com"; // Replace with your email address
+    $recipient = "yms@ymscl.in"; // Replace with your email address
     $name = $_POST['name'];
     $email = $_POST['email'];
     $subject = $_POST['subject'];
