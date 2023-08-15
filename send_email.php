2506e55cc8db316501ac7e66c50b88c5833c8c03
Priyansh Salian <79307288+priyanshpsalian@users.noreply.github.com>
2023-08-15 17:59:26 +0530

    added contact script

---
+++ send_email.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $recipient = "*@gmail.com"; // Replace with your email address
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $headers = "From: $email";
+
+    if (mail($recipient, $subject, $message, $headers)) {
+        echo "Thank you! Your message has been sent.";
+    } else {
+        echo "Sorry, there was an error sending your message.";
+    }
+}
+?>
